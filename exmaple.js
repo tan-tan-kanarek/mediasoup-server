2d0fa9b02b25eadce316045c0381051ab4f13d67
tan-tan-kanarek <Jonathan.Kanarek@Kaltura.com>
2017-06-13 16:16:30 +0300

    Refactoring code

---
+++ exmaple.js
@@ -0,0 +1,84 @@
+'use strict';
+
+//process.env.DEBUG = "mediasoup*";
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
+const {WebRtcServer, RtspServer, ffmpeg} = require('./index.js');
+
+const recordingsPath = path.join(__dirname, "recordings");
+const hostname = os.hostname();
+const streamer = new ffmpeg({
+	enableDebug: true
+});
+
+const webRtcServer = new WebRtcServer();
+webRtcServer
+.listen({
+	enableDebug: true,
+	key: fs.readFileSync('keys/server.key'),
+	cert: fs.readFileSync('keys/server.crt'),
+	port: 3888,
+	path: 'public',
+})
+.on('listen', () => {
+	console.log('Mediasoup demo started');
+})
+.on('web-listen', (port) => {
+	console.log(`Open https://${hostname}:${port} with browser`);
+})
+.on('new-connection', (connection) => {
+	console.log(`New connection [${connection.id}]`);
+	
+	connection
+	.on('error', (err) => {
+		console.log(`Connection [${connection.id}] error: ${err}`);
+	})
+	.on('receive', (action, data) => {
+		console.log(`Connection [${connection.id}] receive [${action}]`);
+	})
+	.on('send', (action, data) => {
+		console.log(`Connection [${connection.id}] send [${action}]`);
+	})
+	.on('new-stream', (stream) => {
+		console.log(`Connection [${connection.id}] peer [${stream.peer.id}] new stream [${stream.id}]`);
+	})
+	.on('ready', (peerConnection) => {
+		console.log(`Connection [${connection.id}] peer [${peerConnection.peer.id}] ready`);
+	})
+	.on('close', (peerId) => {
+		console.log(`Connection [${connection.id}] peer [${peerId}] closed`);
+	})
+	.on('disconnect', (err) => {
+		console.log(`Connection [${connection.id}] signaling disconnected`);
+		connection = null;
+	});
+});
+
+const rtspServer = new RtspServer(webRtcServer);
+rtspServer
+.listen(5000)
+.on('listen', (port) => {
+	console.log(`RTSP server started rtsp://${hostname}:${port}`);
+})
+.on('new-source', (source) => {
+	let rtspUrl = `rtsp://${hostname}:${rtspServer.port}/${source.id}.sdp`;
+	console.log(`New RTSP source ${rtspUrl}`);
+	
+	source.on('enabled', () => {
+		let filepath = `${recordingsPath}/${source.id}.mp4`;
+		let logpath = `${recordingsPath}/${source.id}.log`;
+		
+		console.log(`Recording [${source.id}]: ${filepath}`);
+		
+		let process = streamer.record(rtspUrl, filepath, logpath)
+		.on('error', (err) => {
+			console.error(`Streamer [${source.id}] error: ${err}`);
+		})
+		.on('exit', (code, signal) => {
+			console.log(`Streamer [${source.id}] closed, log: ${logpath}`);
+		});
+	});
+});
